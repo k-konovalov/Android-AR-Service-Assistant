24ed84fde98608f03c6ea6f2b0de9ebb427f4d80
Konovalov Kirill <51828487+konovalov-k@users.noreply.github.com>
2021-01-07 12:56:13 +0300

    Refactor to better readability

--- app/src/main/java/com/konovalovk/serviceassistant/ArFragment.kt
+++ app/src/main/java/com/konovalovk/serviceassistant/ArFragment.kt
@@ -1,21 +1,16 @@
 package com.konovalovk.serviceassistant
 
-import android.media.Image
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.google.ar.core.Config
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.core.Session
 import com.google.ar.core.exceptions.*
-import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Scene
 import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.BaseArFragment
 
 
 class ArFragment: ArFragment() {
@@ -28,26 +23,19 @@ class ArFragment: ArFragment() {
     }
 
     private fun initListeners() {
-        val onUpdateListener = object : Scene.OnUpdateListener {
-            override fun onUpdate(frameTime: FrameTime?) {
-                val image: Image = try {
-                    arSceneView?.arFrame?.acquireCameraImage() ?: return
-                } catch (e: java.lang.Exception) { return }
-                viewModel.findBarcode(image, this@ArFragment, planeDiscoveryController)
-            }
-        }
+        val onUpdateListener = Scene.OnUpdateListener { viewModel.findBarcode(this@ArFragment, planeDiscoveryController) }
 
         arSceneView?.scene?.addOnUpdateListener(onUpdateListener)
 
         setOnTapArPlaneListener { hitResult: HitResult, _: Plane?, motionEvent: MotionEvent? ->
             if (motionEvent?.action == MotionEvent.ACTION_UP)
-                viewModel.onTap(hitResult, this)
+                viewModel.onPlaneTap(hitResult, this)
         }
 
         //arSceneView?.scene?.removeOnUpdateListener()
         viewModel.rvAdapter.portClicked.observe(viewLifecycleOwner,
                 Observer {
-                    viewModel.initDetailView(it ?: return@Observer, requireContext())
+                    viewModel.initAndShowDetailARView(it ?: return@Observer, requireContext())
                 }
         )
     }
